44a259ab8161b332190ba338ec5b04ba219d8b8b
Peter Blunden <peter.blunden@gmail.com>
2015-07-28 17:26:05 +0930

    Camera zoom at startup
    Only works on square arenas

--- Assets/GameBrain.cs
+++ Assets/GameBrain.cs
@@ -4,33 +4,80 @@ using System;
 
 public class GameBrain : MonoBehaviour 
 {
+    public static GameBrain Instance;
+    public GameState State = GameState.Startup;
 
-	// Use this for initialization
-	void Start () 
+    public Camera Camera1;
+    public Camera Camera2;
+
+	void Awake () 
     {
-        GameBrain.Instance = this;   
+        GameBrain.Instance = this;  
+
+        // set each camera to cover half the play area then pan to each player
+        var cameras = GameObject.FindGameObjectsWithTag("MainCamera");
+        var arena = Transform.FindObjectOfType<Arena>();
+
+        // disable camera tracking while intro pan is running
+        Camera1 = cameras[0].GetComponent<Camera>();
+        Camera1.GetComponent<TrackObject>().enabled = false;
+        Camera2 = cameras[1].GetComponent<Camera>();
+        Camera2.GetComponent<TrackObject>().enabled = false;
+
+
+        var aspect = (float)Screen.width / Screen.height;
+        float baseX = (arena.ArenaSize.x + 2) / 4;//  position if width and height are equal
+        float aspectAdjustX;
+        if(aspect < 1)
+        {
+            Debug.Log("Higher than wide");
+            var orth = (arena.ArenaSize.x + 2) / 2;
+            Camera1.orthographicSize = orth / aspect;
+            Camera2.orthographicSize = orth / aspect;
+
+            aspectAdjustX = 0;
+        }
+        else
+        {
+            Debug.Log("Wider than high");
+            Camera1.orthographicSize = (arena.ArenaSize.y + 2) / 2;
+            Camera2.orthographicSize = (arena.ArenaSize.y + 2) / 2;
+
+            // orth size is for y axis so what is it on x?
+            var orthX = Camera1.orthographicSize * aspect;
+            aspectAdjustX = (orthX - Camera1.orthographicSize) / 2; // adjusted for aspect ratio
+        }
+        Camera1.transform.position = new Vector3(-baseX - aspectAdjustX, 0, Camera1.transform.position.z);
+        Camera2.transform.position = new Vector3(baseX + aspectAdjustX, 0, Camera2.transform.position.z);
+
+        WaitAndThenCall(() => PanCameraIntro(), 2);
 	}
-	
-	// Update is called once per frame
+
+    // Update is called once per frame
 	void Update () 
     {
-	
+
 	}
 
-    public static GameBrain Instance;
-    
-
-    public void WaitAndReactivateObject(GameObject obj, float waitSeconds)
+    private void PanCameraIntro()
     {
-        StartCoroutine(CoWaitAndReactivateObject(obj, waitSeconds));
+        var ht = new Hashtable();
+        ht.Add("from", Camera1.orthographicSize);
+        ht.Add("to", 4);
+        ht.Add("time", 4);
+        ht.Add("onupdate", "CamPerspectiveTween");
+        ht.Add("easetype", iTween.EaseType.linear);
+        iTween.ValueTo(gameObject, ht);
+
+        var guy1 = Camera1.gameObject.GetComponent<TrackObject>().target;
+        iTween.MoveTo(Camera1.gameObject, iTween.Hash("x", guy1.transform.position.x, "y", guy1.transform.position.y, "time", 4));
+        var guy2 = Camera2.gameObject.GetComponent<TrackObject>().target;
+        iTween.MoveTo(Camera2.gameObject, iTween.Hash("x", guy2.transform.position.x, "y", guy2.transform.position.y, "time", 4));
     }
 
-    private IEnumerator CoWaitAndReactivateObject(GameObject obj, float waitSeconds)
-    {
-        Debug.Log("Brain waiting...");
-        yield return new WaitForSeconds(waitSeconds);
-        obj.SetActive(true);
-        Debug.Log("Brain done!");
+    void CamPerspectiveTween(float val){
+        Camera1.orthographicSize = val;
+        Camera2.orthographicSize = val;
     }
 
     public void WaitAndThenCall(Action funcToRun, float waitSeconds)
@@ -43,4 +90,13 @@ public class GameBrain : MonoBehaviour
         yield return new WaitForSeconds(waitSeconds);
         funcToRun();
     }
+
+
+}
+
+public enum GameState
+{
+    Startup,
+    GameOn,
+    GameOver
 }
