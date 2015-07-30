dfe0d611b71d9fdda53ed08de8adffe785d6202a
Peter Blunden <peter.blunden@gmail.com>
2015-07-31 01:52:57 +0930

    Added Flamer!

---
+++ Assets/Prefabs/Pickups/Flamer/Flamer.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flamer : Pickup
+{
+    public GameObject FlamePrefab;
+    private float fireDelay = 0.03f;
+    private bool isTriggerDown= false;
+    private float lastFireTime = 0;
+    private PlayerControl player;
+
+    public AudioClip FireSound;
+
+	// Use this for initialization
+	void Start()
+    {
+        BaseStart();
+	}
+	
+	// Update is called once per frame
+	void Update() 
+    {
+        if (isTriggerDown && Time.time - lastFireTime > fireDelay)
+        {
+            Fire();
+            lastFireTime = Time.time;
+        }
+	}
+
+    public override bool IsWeapon()
+    {
+        return true;
+    }
+
+    public override float GetMass()
+    {
+        return 0.1f;
+    }
+
+    private int fullAmmo = 1000;
+    private int ammo = 1000;
+    public override int GetAmmoCount()
+    {
+        return ammo;
+    }
+
+    public override void AddAmmo(int amount)
+    {
+        ammo += amount;
+        if(ammo > fullAmmo)
+            ammo = fullAmmo;
+    }
+
+    public override void OnFireDown(PlayerControl player)
+    {
+        isTriggerDown = true;
+        this.player = player;
+    }
+
+    float aimingError = 5;
+    public void Fire()
+    {
+        if(ammo > 0)
+        {
+
+            var rotation = Quaternion.AngleAxis(player.AimingAngle + Random.Range(-aimingError, aimingError), Vector3.forward);
+            var flame = (GameObject)GameObject.Instantiate(FlamePrefab, player.transform.position, rotation);
+            Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), flame.GetComponent<Collider2D>());
+            var rb = flame.GetComponent<Rigidbody2D>();
+            rb.AddRelativeForce(new Vector2(0, 0.3f), ForceMode2D.Impulse); 
+            rb.AddTorque(Random.Range(-0.1f, 0.1f));
+            AudioSource.PlayClipAtPoint(FireSound, transform.position);
+            ammo--;
+        }
+    }
+
+    public override void OnFireUp(PlayerControl player)
+    {
+        isTriggerDown = false;
+    }
+
+    public override void OnDeselectWeapon()
+    {
+        if(ammo <= 0)
+        {
+            // drop weapon
+            player.RemovePickup(this);
+        }
+    }
+}
---
+++ Assets/Prefabs/Pickups/Flamer/FlamerFlame.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlamerFlame : MonoBehaviour 
+{
+    float timeToFullSize = 1f;
+    float tweenRatioTime = 1f;
+    float fullSize = 2;
+    float birthday;
+
+    SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () 
+    {
+        //transform.localScale = new Vector3(0,0,1);
+        birthday = Time.time;
+        //sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        var time = Time.time - birthday;
+        if(time > timeToFullSize)
+            Destroy(gameObject);
+        else
+        {
+            // size
+            var size = Mathf.Lerp(0, fullSize, time / timeToFullSize);
+
+            // width/height ratio - flames get wider as they grow
+            //var ratio = Mathf.Lerp(5, 1f, time / tweenRatioTime);
+            //transform.localScale = new Vector3(size,size * ratio,1);
+
+//            var r = Mathf.Lerp(Color.yellow.r, Color.red.r, time / timeToFullSize);
+//            var g = Mathf.Lerp(Color.yellow.g, Color.red.g, time / timeToFullSize);
+//            var b = Mathf.Lerp(Color.yellow.b, Color.red.b, time / timeToFullSize);
+//            var a = Mathf.Lerp(1, 0, time / timeToFullSize);
+//            sprite.color = new Vector4(r,g,b,a);
+        }
+	}
+}
